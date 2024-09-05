e3670463a600be9e7303c013da25bdd5c43d469a
bryan <bryan@raksmart.com>
2024-09-05 10:45:11 +0800

    PE-CE-1025: init java sdk
    Signed-off-by: bryan <bryan@raksmart.com>

---
+++ README.md
@@ -0,0 +1,94 @@
+# PetaExpress SDK for Java
+
+
+
+The official PetaExpress SDK for the Java programming language.
+
+## Getting Started
+
+### Preparation
+
+Before your start, please login to [PetaExpress Console](https://console.petaexpress.com/access_keys/) to create a pair of PetaExpress API AccessKey.
+
+___API AccessKey Example:___
+
+``` yaml
+qy_access_key_id: QYACCESSKEYIDEXAMPLE
+qy_secret_access_key: SECRETACCESSKEY
+```
+
+### Installation
+
+Add below dependency in your pom file
+
+```xml
+<dependency>
+    <groupId>com.petaexpress</groupId>
+    <artifactId>petaexpress-sdk-java</artifactId>
+    <version>1.2.2</version>
+</dependency>
+```
+
+For more information, please refer to [maven page](https://search.maven.org/#artifactdetails%7Ccom.petaexpress%7Cpetaexpress-sdk-java%7C1.0.0%7Cjar)
+
+### Usage
+
+Now you are ready to code. You can read the detailed guides in the list below to have a clear understanding.
+
+*Quick start code example*
+
+```java
+public class InstanceTest {
+    private static Logger logger = LoggerFactory.getLogger(InstanceTest.class);
+
+    @Test
+    public void testDescribeInstances() {
+        EnvContext context = new EnvContext("ACCESS_KEY_ID_EXAMPLE", "SECRET_ACCESS_KEY_EXAMPLE");
+        context.setProtocol("https");
+        context.setHost("api.petaexpress.com");
+        context.setPort("443");
+        context.setZone("pek3b");
+        context.setApiLang("zh-cn"); // optional, set return message i18n, default to us-en
+        InstanceService service = new InstanceService(context);
+
+        InstanceService.DescribeInstancesInput input = new InstanceService.DescribeInstancesInput();
+        input.setLimit(1);
+
+        try {
+            InstanceService.DescribeInstancesOutput output = service.describeInstances(input);
+            for (Types.InstanceModel model : output.getInstanceSet()) {
+                System.out.println("==================");
+                System.out.println(model.getInstanceID());
+                System.out.println(model.getInstanceName());
+                System.out.println(model.getImage().getImageID());
+                for (Types.NICVxNetModel vxNetModel : model.getVxNets()) {
+                    System.out.println("==================");
+                    System.out.println(vxNetModel.getVxNetID());
+                    System.out.println(vxNetModel.getVxNetType());
+                }
+            }
+        } catch (QCException e) {
+            e.printStackTrace();
+        }
+    }
+}
+```
+
+
+- [Configuration Guide](docs/configuration.md)
+- [PetaExpress Service Usage Guide](docs/petaexpress_service_usage.md)
+
+Checkout our [releases](https://github.com/peta-express/cloud-sdk-java/releases) and [change logs](https://github.com/peta-express/cloud-sdk-java/blob/master/CHANGELOGS) for information about the latest features, bug fixes and new ideas.
+
+## Reference Documentations
+
+- [PetaExpress Documentation Overview](https://docs.petaexpress.com)
+- [PetaExpress IaaS APIs](https://docs.petaexpress.com/product/api/)
+
+## Contributing
+
+This repository was automatically generated, please contribute to [`Snips`](https://github.com/peta-express/snips) instead.
+
+## LICENSE
+
+The Apache License (Version 2.0, January 2004).
---
+++ src/main/java/com/petaexpress/sdk/service/MiscService.java
@@ -0,0 +1,411 @@
+// +-------------------------------------------------------------------------
+// | Copyright (C) 2016 RAKSmart, Inc.
+// +-------------------------------------------------------------------------
+// | Licensed under the Apache License, Version 2.0 (the "License");
+// | you may not use this work except in compliance with the License.
+// | You may obtain a copy of the License in the LICENSE file, or at:
+// |
+// | http://www.apache.org/licenses/LICENSE-2.0
+// |
+// | Unless required by applicable law or agreed to in writing, software
+// | distributed under the License is distributed on an "AS IS" BASIS,
+// | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// | See the License for the specific language governing permissions and
+// | limitations under the License.
+// +-------------------------------------------------------------------------
+
+package com.petaexpress.sdk.service;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.petaexpress.sdk.config.EnvContext;
+import com.petaexpress.sdk.constants.QCConstant;
+import com.petaexpress.sdk.model.OutputModel;
+import com.petaexpress.sdk.request.ResponseCallBack;
+import com.petaexpress.sdk.request.ResourceRequestFactory;
+import com.petaexpress.sdk.exception.QCException;
+import com.petaexpress.sdk.model.IaasParamBody;
+import com.petaexpress.sdk.service.Types.*;
+import com.petaexpress.sdk.utils.QCStringUtil;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MiscService {
+
+    private EnvContext envContext;
+
+    private String zone;
+
+    public MiscService(EnvContext envContext) {
+        this.envContext = envContext;
+    }
+
+    public MiscService(EnvContext envContext, String zone) {
+        this.envContext = envContext;
+        this.zone = zone;
+    }
+        
+    /**
+      * @param input Request parameters and headers in the class
+      * @return GetQuotaLeftOutput Response body and headers in the class
+      * @throws QCException IOException or network error
+      *
+      * <a href=https://docs.petaexpress.com/product/api/action/misc/get_quota_left.html>Documentation URL</a>
+      */
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    public GetQuotaLeftOutput getQuotaLeft(GetQuotaLeftInput input) throws QCException {
+        if (input == null) {
+            input = new GetQuotaLeftInput();
+        }
+        Map context = new HashMap();
+        context.put(QCConstant.ENV_CONTEXT_KEY, this.envContext);
+        context.put("OperationName", "GetQuotaLeft");
+        context.put("APIName", "GetQuotaLeft");
+        context.put("ServiceName", "GetQuotaLeft");
+        context.put("RequestMethod", "GET");
+        context.put("RequestURI", "/iaas/GetQuotaLeft");
+
+        input.setAction("GetQuotaLeft");
+        if (QCStringUtil.isEmpty(this.zone)) {
+            input.setZone(this.envContext.getZone());
+        } else {
+            input.setZone(this.zone);
+        }
+
+        Object backModel = ResourceRequestFactory.getResourceRequest().sendApiRequest(context, input,GetQuotaLeftOutput.class);
+        if(backModel != null){
+            return (GetQuotaLeftOutput)backModel;
+        }
+        return null;
+    }
+
+     /**
+       * @param input Request parameters and headers in the class
+       * @param callback Response calls back when success
+       * @throws QCException IOException or network error
+       *
+       * <a href=https://docs.petaexpress.com/product/api/action/misc/get_quota_left.html>Documentation URL</a>
+       */
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    public void getQuotaLeft(GetQuotaLeftInput input, ResponseCallBack<GetQuotaLeftOutput> callback) throws QCException {
+        if (input == null) {
+            input = new GetQuotaLeftInput();
+        }
+        Map context = new HashMap();
+        context.put(QCConstant.ENV_CONTEXT_KEY, this.envContext);
+        context.put("OperationName", "GetQuotaLeft");
+        context.put("APIName", "GetQuotaLeft");
+        context.put("ServiceName", "GetQuotaLeft");
+        context.put("RequestMethod", "GET");
+        context.put("RequestURI", "/iaas/GetQuotaLeft");
+
+        input.setAction("GetQuotaLeft");
+        if (QCStringUtil.isEmpty(this.zone)) {
+            input.setZone(this.envContext.getZone());
+        } else {
+            input.setZone(this.zone);
+        }
+
+        if(callback == null) {
+            throw new QCException("callback can't be null");
+        }
+
+        ResourceRequestFactory.getResourceRequest().sendApiRequestAsync(context,input,callback);
+    }
+
+    public static class GetQuotaLeftInput extends IaasParamBody {
+        private List<String> resourceTypes;
+
+        @JsonProperty(value = "resource_types")
+        public void setResourceTypes(List<String> resourceTypes) {
+            this.resourceTypes = resourceTypes;
+        }
+
+        @JsonProperty(value = "resource_types")
+        public List<String> getResourceTypes() {
+            return this.resourceTypes;
+        }
+
+        // Required
+        private String zone;
+
+        @JsonProperty(value = "zone")
+        public void setZone(String zone) {
+            this.zone = zone;
+        }
+
+        @JsonProperty(value = "zone")
+        public String getZone() {
+            return this.zone;
+        }
+
+        public String validateParam() throws QCException {
+            if (QCStringUtil.isEmpty(this.getZone())) {
+                throw new QCException("Zone is required");
+            }
+            return null;
+        }
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class GetQuotaLeftOutput extends OutputModel {
+        private String action;
+
+        @JsonProperty(value = "action")
+        public void setAction(String action) {
+            this.action = action;
+        }
+
+        @JsonProperty(value = "action")
+        public String getAction() {
+            return this.action;
+        }
+
+        private List<QuotaLeftModel> quotaLeftSet;
+
+        @JsonProperty(value = "quota_left_set")
+        public void setQuotaLeftSet(List<QuotaLeftModel> quotaLeftSet) {
+            this.quotaLeftSet = quotaLeftSet;
+        }
+
+        @JsonProperty(value = "quota_left_set")
+        public List<QuotaLeftModel> getQuotaLeftSet() {
+            return this.quotaLeftSet;
+        }
+
+        private Integer retCode;
+
+        @JsonProperty(value = "ret_code")
+        public void setRetCode(Integer retCode) {
+            this.retCode = retCode;
+        }
+
+        @JsonProperty(value = "ret_code")
+        public Integer getRetCode() {
+            return this.retCode;
+        }
+
+    }
+        
+    /**
+      * @param input Request parameters and headers in the class
+      * @return GetResourceLimitOutput Response body and headers in the class
+      * @throws QCException IOException or network error
+      *
+      * <a href=https://docs.petaexpress.com/product/api/action/misc>Documentation URL</a>
+      */
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    public GetResourceLimitOutput getResourceLimit(GetResourceLimitInput input) throws QCException {
+        if (input == null) {
+            input = new GetResourceLimitInput();
+        }
+        Map context = new HashMap();
+        context.put(QCConstant.ENV_CONTEXT_KEY, this.envContext);
+        context.put("OperationName", "GetResourceLimit");
+        context.put("APIName", "GetResourceLimit");
+        context.put("ServiceName", "GetResourceLimit");
+        context.put("RequestMethod", "GET");
+        context.put("RequestURI", "/iaas/GetResourceLimit");
+
+        input.setAction("GetResourceLimit");
+        if (QCStringUtil.isEmpty(this.zone)) {
+            input.setZone(this.envContext.getZone());
+        } else {
+            input.setZone(this.zone);
+        }
+
+        Object backModel = ResourceRequestFactory.getResourceRequest().sendApiRequest(context, input,GetResourceLimitOutput.class);
+        if(backModel != null){
+            return (GetResourceLimitOutput)backModel;
+        }
+        return null;
+    }
+
+     /**
+       * @param input Request parameters and headers in the class
+       * @param callback Response calls back when success
+       * @throws QCException IOException or network error
+       *
+       * <a href=https://docs.petaexpress.com/product/api/action/misc>Documentation URL</a>
+       */
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    public void getResourceLimit(GetResourceLimitInput input, ResponseCallBack<GetResourceLimitOutput> callback) throws QCException {
+        if (input == null) {
+            input = new GetResourceLimitInput();
+        }
+        Map context = new HashMap();
+        context.put(QCConstant.ENV_CONTEXT_KEY, this.envContext);
+        context.put("OperationName", "GetResourceLimit");
+        context.put("APIName", "GetResourceLimit");
+        context.put("ServiceName", "GetResourceLimit");
+        context.put("RequestMethod", "GET");
+        context.put("RequestURI", "/iaas/GetResourceLimit");
+
+        input.setAction("GetResourceLimit");
+        if (QCStringUtil.isEmpty(this.zone)) {
+            input.setZone(this.envContext.getZone());
+        } else {
+            input.setZone(this.zone);
+        }
+
+        if(callback == null) {
+            throw new QCException("callback can't be null");
+        }
+
+        ResourceRequestFactory.getResourceRequest().sendApiRequestAsync(context,input,callback);
+    }
+
+    public static class GetResourceLimitInput extends IaasParamBody {
+        private Integer volumeType;
+
+        @JsonProperty(value = "volume_type")
+        public void setVolumeType(Integer volumeType) {
+            this.volumeType = volumeType;
+        }
+
+        @JsonProperty(value = "volume_type")
+        public Integer getVolumeType() {
+            return this.volumeType;
+        }
+
+        // Required
+        private String zone;
+
+        @JsonProperty(value = "zone")
+        public void setZone(String zone) {
+            this.zone = zone;
+        }
+
+        @JsonProperty(value = "zone")
+        public String getZone() {
+            return this.zone;
+        }
+
+        public String validateParam() throws QCException {
+            if (QCStringUtil.isEmpty(this.getZone())) {
+                throw new QCException("Zone is required");
+            }
+            return null;
+        }
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class GetResourceLimitOutput extends OutputModel {
+        private String action;
+
+        @JsonProperty(value = "action")
+        public void setAction(String action) {
+            this.action = action;
+        }
+
+        @JsonProperty(value = "action")
+        public String getAction() {
+            return this.action;
+        }
+
+        private Integer maxSize;
+
+        @JsonProperty(value = "max-size")
+        public void setMaxSize(Integer maxSize) {
+            this.maxSize = maxSize;
+        }
+
+        @JsonProperty(value = "max-size")
+        public Integer getMaxSize() {
+            return this.maxSize;
+        }
+
+        private Integer minSize;
+
+        @JsonProperty(value = "min-size")
+        public void setMinSize(Integer minSize) {
+            this.minSize = minSize;
+        }
+
+        @JsonProperty(value = "min-size")
+        public Integer getMinSize() {
+            return this.minSize;
+        }
+
+        private ResourceLimitsModel resourceLimits;
+
+        @JsonProperty(value = "resource_limits")
+        public void setResourceLimits(ResourceLimitsModel resourceLimits) {
+            this.resourceLimits = resourceLimits;
+        }
+
+        @JsonProperty(value = "resource_limits")
+        public ResourceLimitsModel getResourceLimits() {
+            return this.resourceLimits;
+        }
+
+        private Integer retCode;
+
+        @JsonProperty(value = "ret_code")
+        public void setRetCode(Integer retCode) {
+            this.retCode = retCode;
+        }
+
+        @JsonProperty(value = "ret_code")
+        public Integer getRetCode() {
+            return this.retCode;
+        }
+
+        private Integer step;
+
+        @JsonProperty(value = "step")
+        public void setStep(Integer step) {
+            this.step = step;
+        }
+
+        @JsonProperty(value = "step")
+        public Integer getStep() {
+            return this.step;
+        }
+
+        private List<String> vxNetSubnets;
+
+        @JsonProperty(value = "vxnet_subnets")
+        public void setVxNetSubnets(List<String> vxNetSubnets) {
+            this.vxNetSubnets = vxNetSubnets;
+        }
+
+        @JsonProperty(value = "vxnet_subnets")
+        public List<String> getVxNetSubnets() {
+            return this.vxNetSubnets;
+        }
+
+        private Integer vxNetVersion;
+
+        @JsonProperty(value = "vxnet_version")
+        public void setVxNetVersion(Integer vxNetVersion) {
+            this.vxNetVersion = vxNetVersion;
+        }
+
+        @JsonProperty(value = "vxnet_version")
+        public Integer getVxNetVersion() {
+            return this.vxNetVersion;
+        }
+
+    }}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
